397989de226759365627f10f7f963899f2a35cb3
martin <martin@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2013-09-01 16:50:18 +0000

    Codebereinigung
    git-svn-id: https://svn.borger.co.at/repos/redeye/playstream/trunk/playstream@2025 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- main.cpp
+++ main.cpp
@@ -41,14 +41,6 @@ public:
     }
 };
 
-#if 0
-template<typename T> VarIO & operator<<( VarIO &io, const T & sout )
-{
-    if( io.out )
-	*io.out << sout;
-    return io;
-}
-#endif
 
 using namespace std;
 using namespace Tools;
@@ -91,17 +83,6 @@ int main( int argc, char **argv )
     }
 
     int load_count = 1;
-
-    /*
-    std::string s;
-
-    while( getline( std::cin, s ) )
-      {
-	VOUT(0)( "%s\n", s );
-      }
-
-	return 0;
-    */
 
 	cgicc::Cgicc cgi;
 	VarIO IO( &cout );
